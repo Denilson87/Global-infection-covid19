3534207c86be6299e506b549d8e38fa606db61f4
Denilson Mucavele <77127716+Denilson87@users.noreply.github.com>
2021-10-28 05:43:20 -0700

    identing src

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -18,6 +18,7 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
           lastUpdate={lastUpdate}
           cardSubtitle="Number of active cases from COVID-19."
         />
+            
         <CardComponent
           className={styles.recovered}
           cardTitle="Recovered"
